50cefc69cd5382c2065d0d08dcf9bea7bd133fbe
Doug A <douganderson444@gmail.com>
2022-02-04 10:52:09 -0400

    gh page env

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+
+const production = !process.env.ROLLUP_WATCH;
+
+console.log({ production });
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +13,19 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		}),
+		paths: {
+			// change below to your repo name
+			base: process.env.NODE_ENV !== 'production' ? '' : '/autobase-kit'
+		},
+		vite: {
+			define: {
+				'process.env': process.env
+			}
+		}
 	}
 };
 
